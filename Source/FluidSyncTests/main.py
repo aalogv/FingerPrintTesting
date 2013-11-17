5d311718f804ba493201298130c4482d8a283a98
ded101 <vovaded101@gmail.com>
2013-11-18 01:52:30 +0200

    not working version

--- Source/FluidSyncTests/main.py
+++ Source/FluidSyncTests/main.py
@@ -9,7 +9,7 @@ hashBuild = "d:\\TestCodec\\FingerPrint\\FingerprintTest\\HashBuild.exe"
 hbTool = "d:\\TestCodec\\FingerPrint\\FingerprintTest\\HBTool.exe"
 hashSearch = "d:\\TestCodec\\FingerPrint\\FingerprintTest\\HashSearch.exe"
 
-dirName = "d:\\TestCodec\\FingerPrint\\FingerprintTest\\"
+aifSetAll = "d:\\TestCodec\\FingerPrint\\FingerprintTest\\AifSetAll\\"
 aifSet = "d:\\TestCodec\\FingerPrint\\FingerprintTest\\AifSet\\"
 baseName = aifSet + "fpBase.hbs"
 baseNameAdditional = aifSet + "fpBase.nbs"
@@ -22,10 +22,10 @@ os.chdir(aifSet)
 
 
 # Читаем файлы из директории
-for dirname, dirnames, filenames in os.walk(aifSet):
+for dirname, dirnames, filenames in os.walk(aifSetAll):
     for filename in filenames:
         if fnmatch.fnmatch(filename, '*.aif'):
-            path = os.path.join(aifSet, filename)
+            path = os.path.join(aifSetAll, filename)
             track = Track()
             track.path = path
             tracks.append(track)
@@ -37,11 +37,7 @@ for dirname, dirnames, filenames in os.walk(aifSet):
 #    subprocess.call([sox, track.path, wavFileName])
 #    track.path = wavFileName
 
-# Определям длины файлов
 
-for track in tracks:
-    track.audioLength = int(float(subprocess.check_output([sox, "--i", "-D", track.path]).strip()))
-    #print(track.path + " - " + str(track.audioLength))
 
 #Делаем хеши полных файлов
 for track in tracks:
@@ -64,14 +60,32 @@ for track in tracks:
 
     idIndex = int(out.find("contains id = "))
     track.fingerPrintId = int(out[idIndex+14:].split('\\')[0]) # 14 - длина строки "contains id = "
+    print()
     print(track.hashPath)
+    print("FP_ID = " + str(track.fingerPrintId))
     #print(track.hashPath)
 
+tracks = list()
+
+# Читаем файлы из директории
+for dirname, dirnames, filenames in os.walk(aifSet):
+    for filename in filenames:
+        if fnmatch.fnmatch(filename, '*.aif'):
+            path = os.path.join(aifSet, filename)
+            track = Track()
+            track.path = path
+            tracks.append(track)
+
+# Определям длины файлов
+
+for track in tracks:
+    track.audioLength = int(float(subprocess.check_output([sox, "--i", "-D", track.path]).strip()))
+    #print(track.path + " - " + str(track.audioLength))
 
 # Режем файлы на куски по 5с с интервалом 0.1с
 for track in tracks:
-    i = 0
-    while i < 10 * min(maxLengthCheck, track.audioLength - 5):
+    i = 10
+    while i < 10 * min(maxLengthCheck,9): #track.audioLength - 5):
         i += 1
         cutFragmentName = os.path.splitext(track.path)[0] + "_" + str(i*100) + ".aif"
         subprocess.check_output([sox, track.path, cutFragmentName, "trim", str(i/10), "5"])
@@ -80,12 +94,16 @@ for track in tracks:
         subprocess.check_output([hashBuild, cutFragmentName, cutHashName])
 
         track.peacesHashes.append(cutHashName)
+        #track.peacesHashes[cutHashName] = i*100
         #print(track.peacesHashes[track.peacesHashes.count - 1])
 
 
 
 
 time.sleep(1)
+# Запись результатов в файл
+#resultFile = open("result.txt", 'w')
+
 #Делаем поиск по маленьким хешам в базе
 for track in tracks:
     for peaceHashPath in track.peacesHashes:
@@ -95,20 +113,24 @@ for track in tracks:
 
         print()
         print("Searching " + peaceHashPath)
-        # print(result)
+        print(result)
         idIndex = int(result.find("->"))
         if(idIndex>0):
             fpId = result[idIndex+6:].split(':')[0]
+            print(fpId)
             fpId = int(fpId.lstrip('0'))
+
             print("FP_ID = " + str(fpId))
 
             shiftIndex = int(result.find("shift = "))
             shift = int(result[shiftIndex+8:].split(' ')[0])
             print("Shift = " + str(shift))
+            #resultFile.write(peaceHashPath+ "," + str(fpId) + "," + str(shift) + "," + track.peacesHashes[peaceHashPath])
         else:
             print("Search failed")
+            #resultFile.write(peaceHashPath+ "," + "Failed" + "," + "Failed" + "," + track.peacesHashes[peaceHashPath])
 
         #print(subprocess.check_output([hashSearch, "-w", peaceHashPath, baseName,result]))
 
-
+#resultFile.close()
 
